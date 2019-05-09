9a995ec6b116f355652c467ba986e67d7495bc32
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 20:45:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
 def reverse_each_word(sentence)
-  new_sentence = sentence.to_a
-  reverse_sentence
-  new_sentenc.each do |word|
-    reverse_sentence << word.reverse
-    return reverse_sentence
+  
 end
   
